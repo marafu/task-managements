130ab371c6243d8415f3dc3a6c56a6ba3aaf3e7e
Matheus Assis <matheusassissilva20@gmail.com>
2023-10-09 20:32:53 -0300

    refactor: ajust limit lenght

--- tests/Description.test.ts
+++ tests/Description.test.ts
@@ -5,4 +5,14 @@ describe("Classe Description", () => {
     const description = new Description("Uma breve descrição");
     expect(description.getValue()).toBe("Uma breve descrição");
   });
+
+  test("Não deve criar uma descrição seja menor que 2 caracteres", () => {
+    const description = new Description("U");
+    expect(description.isValid()).toBeFalsy();
+  });
+
+  test("Não deve criar uma descrição seja maior que 1024", () => {
+    const description = new Description("uma descrição muito grande", 20);
+    expect(description.isValid()).toBeFalsy();
+  });
 });
