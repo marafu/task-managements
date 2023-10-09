dfec3f5876fdceb606a0ff0292e4fa7ea535ea94
Matheus Assis <matheusassissilva20@gmail.com>
2023-10-09 20:12:38 -0300

    feat: add Title

---
+++ src/Title.ts
@@ -0,0 +1,18 @@
+export class Title {
+  private value: string;
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  isValid(): boolean {
+    if (this.value.length < 2 || this.value.length > 32) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+}
