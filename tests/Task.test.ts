aed1a22b5e4b8862a76207e835415dbc903cbd3d
Matheus Assis <matheusassissilva20@gmail.com>
2023-10-09 20:17:24 -0300

    refactor: remove title tests of Task

--- tests/Task.test.ts
+++ tests/Task.test.ts
@@ -1,17 +1,8 @@
 import { Task } from "../src/Task";
 
-describe("Tarefa", () => {
+describe("Classe Task", () => {
   test("Deve criar uma tarefa", function () {
-    const task = new Task("Tarefa 1", "Descrição da tarefa 1", "Novo");
-
+    const task = Task.create("Tarefa 1", "Descrição da tarefa 1", "Novo");
     expect(task).toBeTruthy();
-  });
-
-  test("Não deve criar uma task que tenha o titulo vazio", function () {
-    const task = new Task("", "Descrição da tarefa 1", "Novo");
-
-    expect(() => {
-      task.create();
-    }).toThrowError(new Error("The title cannot be empty"));
   });
 });
