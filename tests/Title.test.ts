54b8ee382b34915d3d093a3fae0ad6de2d9a22ae
Matheus Assis <matheusassissilva20@gmail.com>
2023-10-09 20:13:02 -0300

    test: add Title tests

---
+++ tests/Title.test.ts
@@ -0,0 +1,25 @@
+import { Title } from "../src/Title";
+
+describe("Classe Title", () => {
+  test("Deve criar um titulo", () => {
+    const title = new Title("Titulo");
+    expect(title.getValue()).toBe("Titulo");
+  });
+
+  test("Não deve criar um titulo vazio", function () {
+    const title = new Title("");
+    expect(title.isValid()).toBeFalsy();
+  });
+
+  test("Não deve criar um titulo que seja maior que 32 caracteres", function () {
+    const title = new Title(
+      "uma task que contem um numero muito alto de caracteres"
+    );
+    expect(title.isValid()).toBeFalsy();
+  });
+
+  test("Não deve criar um titulo que seja menor que 2 caracteres", function () {
+    const title = new Title("A");
+    expect(title.isValid()).toBeFalsy();
+  });
+});
