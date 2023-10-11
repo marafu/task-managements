682d42e64972461ecbd1beaa36a74aac069373b5
Matheus Assis <matheusassissilva20@gmail.com>
2023-10-11 02:56:43 -0300

    feat: add controls for change status

--- src/Task.ts
+++ src/Task.ts
@@ -1,17 +1,61 @@
+import { BusinessError, ChangeStatusError } from "./BusinessErrors";
+import { Description } from "./Description";
 import { Title } from "./Title";
 
 export class Task {
   private title: Title;
-  private description: string;
+  private description: Description;
   private status: string;
 
-  private constructor(title: Title, description: string, status: string) {
+  private constructor(title: Title, description: Description, status: string) {
     this.title = title;
     this.description = description;
     this.status = status;
   }
 
-  static create(title: string, description: string, status: string) {
-    return new Task(new Title(title), description, status);
+  static create(title: string, description: string): Task {
+    return new Task(new Title(title), new Description(description), "Todo");
+  }
+
+  getTitle(): string {
+    return this.title.getValue();
+  }
+
+  getDescription(): string {
+    return this.description.getValue();
+  }
+
+  getStatus(): string {
+    return this.status;
+  }
+
+  changeStatusInProgress() {
+    if (this.status === "Cancelled")
+      throw new ChangeStatusError({
+        message: "Não foi possível alterar o status de uma tarefa cancelada",
+      });
+    if (this.status === "Done")
+      throw new ChangeStatusError({
+        message: "Não foi possível alterar o status de Done para In Progress",
+      });
+    if (this.status === "Todo") {
+      this.status = "In Progress";
+    }
+  }
+
+  changeStatusDone() {
+    if (this.status === "Cancelled")
+      throw new ChangeStatusError({
+        message: "Não foi possível alterar o status de uma tarefa cancelada",
+      });
+    if (this.status === "In Progress") {
+      this.status = "Done";
+    }
+  }
+
+  cancelTask() {
+    if (this.status != "") {
+      this.status = "Cancelled";
+    }
   }
 }
